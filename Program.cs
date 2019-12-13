8c1e39720eb16174f8e118d3ae3f0bc1e33464fc
Ahmed Bilal <ahmed.bilal@netrevelation.com>
2019-12-13 11:50:27 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+ class First 
+
+{
+
+    public int Greaternum(int x, int y)
+    {
+        int result;
+
+        if (x > y)
+
+        {
+            result = x;
+            return result;
+        }
+
+        else
+
+        {
+            result = y;
+            return result;
+        }
+        
+    }
+
+    public static void Main()
+
+    {
+
+        int final;
+
+        First myobj = new First();
+
+        final = myobj.Greaternum(10, 100);
+
+        System.Console.WriteLine(final);
+
+      //  System.Console.WriteLine(myobj.Greaternum(5, 9));
+
+    }
+
+
+}
